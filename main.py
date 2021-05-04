c63757afd7e5334da1b37aca839c51580129a209
sethpoly <emailspolyniak@gmail.com>
2021-05-04 16:58:53 -0400

    Added loop to rerun every X minutes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Simple script to reload your pythonanywhere application so the data refreshes'
 import requests
 import os
+import time
 from dotenv import load_dotenv
 
 # load local env file
@@ -9,14 +10,22 @@ load_dotenv()
 username = os.environ['user']
 api_token = os.environ['api_token']
 domain_name = os.environ['domain_name']
+timer = 1800;  # Seconds before rerun
 
-response = requests.post(
-    'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain_name}/reload/'.format(
-        username=username, domain_name=domain_name
-    ),
-    headers={'Authorization': 'Token {token}'.format(token=api_token)}
-)
-if response.status_code == 200:
-    print('reloaded OK')
-else:
-    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+while True:
+    try:
+        response = requests.post(
+            'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain_name}/reload/'.format(
+                username=username, domain_name=domain_name
+            ),
+            headers={'Authorization': 'Token {token}'.format(token=api_token)}
+        )
+        if response.status_code == 200:
+            print(f'{time.strftime("%H:%M:%S", time.localtime())} - Successfully reloaded {domain_name}.')
+        else:
+            print('Error: Status code {}: {!r}'.format(response.status_code, response.content))
+        print(f'Will reload in {timer / 60} minutes.')
+        time.sleep(timer)
+    except KeyboardInterrupt:
+        print('Interrupted... Ending application')
+        exit()
